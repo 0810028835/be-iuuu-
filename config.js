461f4af48e0a6c4a86fdd6fd685fac562baee89d
0810028835 <86754584+0810028835@users.noreply.github.com>
2021-07-01 22:40:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'DÂU iu ới ời🍓!',
+    introDesc: `Em muon hon doi moi anh `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Là một gã thợ săn,em sẽ săn sóc anh cả đời❤️ ',
+    desc: 'Anh à! Em cực thích màu blue, because i love u everyday💓  ',
+    btnYes: 'Vẫn cứ là iuu bé <33',
+    btnNo: 'Không, bây bi à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em điii <3',
+    reply: 'Yêu thì yêu mà không yêu thì vẫn yêuuuu <33333333',
+    mess: 'Em biết mà 🥰. Yêu chim nonnn nhiều nhiều 😘😘',
+    messDesc: 'Công chúa nhớ nô tỳyy rùi, mau đến đây cho coongg túa ôm chym non điii !!!',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/100028013123511' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
